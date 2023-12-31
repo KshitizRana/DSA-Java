e1f4a82c7513de6d0fe18ca746401969a20be334
KshitizRana <ranakshitizgm@gmail.com>
2023-12-31 11:19:45 +0530

    Add Assignment Armstrong.java

---
+++ firstjavaprogram/Main.java/src/com/kshitiz/Assignments/Armstrong.java
@@ -0,0 +1,14 @@
+package com.kshitiz.Assignments;
+
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Start: ");
+        int num1 = input.nextInt();
+        System.out.println("Enter End: ");
+        int num2 = input.nextInt();
+
+    }
+}
